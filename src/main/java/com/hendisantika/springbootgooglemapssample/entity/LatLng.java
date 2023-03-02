6f080fa2ac0c3aed95a3f2470f2ebcf94d5f63d5
hendisantika <hendisantika@yahoo.co.id>
2023-03-03 06:49:02 +0700

    #9 - Add LatLng Entity ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootgooglemapssample/entity/LatLng.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootgooglemapssample.entity;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-googlemaps-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/3/23
+ * Time: 06:48
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+public class LatLng {
+    private double lat;
+    private double lng;
+}
