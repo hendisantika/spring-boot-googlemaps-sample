bcb3b5e046280e44c2f17b9723fb4fc082db9ca0
hendisantika <hendisantika@yahoo.co.id>
2023-03-03 06:42:12 +0700

    #1 - Initial Commit ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootgooglemapssample/SpringBootGooglemapsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootgooglemapssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGooglemapsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGooglemapsSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootgooglemapssample/SpringBootGooglemapsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootgooglemapssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGooglemapsSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
