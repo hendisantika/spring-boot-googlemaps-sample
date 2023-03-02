341ca9ff8fa41d909f233d544dd81fdf6c3c09ce
hendisantika <hendisantika@yahoo.co.id>
2023-03-03 06:50:46 +0700

    #13 - Add LatLngDTO ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootgooglemapssample/dto/LatLngDTO.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootgooglemapssample.dto;
+
+import lombok.ToString;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-googlemaps-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/3/23
+ * Time: 06:50
+ * To change this template use File | Settings | File Templates.
+ */
+@ToString
+public class LatLngDTO {
+    public double lat;
+    public double lng;
+}
