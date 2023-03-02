340de874d53e28246c970dd0f1788397534fbf75
hendisantika <hendisantika@yahoo.co.id>
2023-03-03 06:52:19 +0700

    #17 - Add PlaceSenseRepository ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootgooglemapssample/repository/PlaceSenseRepository.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springbootgooglemapssample.repository;
+
+import com.hendisantika.springbootgooglemapssample.entity.PlaceSense;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-googlemaps-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/3/23
+ * Time: 06:52
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PlaceSenseRepository extends CrudRepository<PlaceSense, Long> {
+}
