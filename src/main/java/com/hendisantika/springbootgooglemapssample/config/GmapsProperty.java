4065fac3ff2081c8950aa44d843ff96246762ed2
hendisantika <hendisantika@yahoo.co.id>
2023-03-03 06:58:47 +0700

    #25 - Add GmapsProperty ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootgooglemapssample/config/GmapsProperty.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springbootgooglemapssample.config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-googlemaps-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/3/23
+ * Time: 06:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class GmapsProperty {
+    public static final String GMAPS_API_KEY = "gmaps.api.key";
+}
