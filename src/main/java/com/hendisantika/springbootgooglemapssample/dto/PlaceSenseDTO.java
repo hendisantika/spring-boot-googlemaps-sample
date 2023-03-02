c64c72901a5432e86a4c537cc8d52deeb3c21990
hendisantika <hendisantika@yahoo.co.id>
2023-03-03 06:51:07 +0700

    #14 - Add PlaceSenseDTO ✏️ 📗 🐳

---
+++ src/main/java/com/hendisantika/springbootgooglemapssample/dto/PlaceSenseDTO.java
@@ -0,0 +1,22 @@
+package com.hendisantika.springbootgooglemapssample.dto;
+
+import lombok.ToString;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-googlemaps-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 3/3/23
+ * Time: 06:50
+ * To change this template use File | Settings | File Templates.
+ */
+@ToString
+public class PlaceSenseDTO {
+    public Long id;
+    public String placeName;
+    public String placeCountryCode;
+    public LatLngDTO placeLatLng;
+    public String sense;
+}
