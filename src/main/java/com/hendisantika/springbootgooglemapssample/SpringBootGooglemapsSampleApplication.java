aa5392b7afaab7818abd9774f4ef85320135f7ad
hendisantika <hendisantika@yahoo.co.id>
2023-03-03 07:03:24 +0700

    #30 - Add setModelAttributes in Main App ✏️ 📗 🐳

--- src/main/java/com/hendisantika/springbootgooglemapssample/SpringBootGooglemapsSampleApplication.java
+++ src/main/java/com/hendisantika/springbootgooglemapssample/SpringBootGooglemapsSampleApplication.java
@@ -2,12 +2,22 @@ package com.hendisantika.springbootgooglemapssample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @SpringBootApplication
+@ControllerAdvice
 public class SpringBootGooglemapsSampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootGooglemapsSampleApplication.class, args);
     }
 
+    @ModelAttribute
+    public void setModelAttributes(Model model) {
+        model.addAttribute("bootstrapVersion", "4.1.3");
+        model.addAttribute("jqueryVersion", "3.3.1-1");
+        model.addAttribute("faVersion", "5.3.1");
+    }
 }
